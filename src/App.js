74209540487b8649a935e289a481021ec6628d95
CHACHA MOHAMMED NADJIB <139798416+Nadjibochacha@users.noreply.github.com>
2024-12-30 10:05:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Weather from './components/weather';
 
 function App() {
   return (
-    <div className="App w-screen h-screen bg-cyan-300 flex justify-center items-center overflow-x-hidden">
+    <div className="App pt-10 w-screen h-screen bg-cyan-300 flex justify-center items-center overflow-x-hidden">
       <Weather/>
     </div>
   );
