e40e3503f41713ee268f4723e679395f78b040a2
CHACHA MOHAMMED NADJIB <139798416+Nadjibochacha@users.noreply.github.com>
2024-12-30 10:07:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Weather from './components/weather';
-
+import Footer from './components/footer'
 function App() {
   return (
-    <div className="App pt-10 w-screen h-screen bg-cyan-300 flex justify-center items-center overflow-x-hidden">
+    <div className="App pt-10 w-screen h-screen bg-cyan-300 grid grid-cols-1 justify-items-center place-items-center overflow-x-hidden">
       <Weather/>
+      <Footer/>
     </div>
   );
 }
